5d458bad3c3745b4716c301aea3a9e6b96db7d28
Adi Satria Sejati <117582840+AdiSatria15@users.noreply.github.com>
2023-05-17 08:40:21 +0700

    Update Page Background Video.js

--- JS/Page Background Video.js
+++ JS/Page Background Video.js
@@ -22,9 +22,9 @@ pauseButton.addEventListener("click", function () {
   vid.classList.toggle("stopfade");
   if (vid.paused) {
     vid.play();
-    pauseButton.innerHTML = "Pause";
+    pauseButton.innerHTML = "Jeda";
   } else {
     vid.pause();
-    pauseButton.innerHTML = "Play";
+    pauseButton.innerHTML = "Putar";
   }
 });
